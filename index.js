03ef380e4d2067ec6acf3fad591e9dc4ecabdd12
Joeling-M <joseph.myc@gmail.com>
2021-02-27 18:22:06 -0500

    Functionality and formatting update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("navBar").style.width = "25vw";
-    document.getElementById("main").style.marginLeft = "25vw";
+    document.getElementById("navBar").style.width = "20vw";
+    document.getElementById("main").style.marginLeft = "20vw";
 }
   
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
