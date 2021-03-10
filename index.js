0d5de33933b36406dc53e9608f42080905609781
Joeling-M <joseph.myc@gmail.com>
2021-03-10 00:21:55 -0500

    mobile update

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ function openNav() {
         document.getElementById("navBar").style.width = "20vw";
         document.getElementById("main").style.marginLeft = "20vw";
     }
-    
 }
   
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
-    console.log(screen.availWidth);
     document.getElementById("navBar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+}
+
+function closeNavMobile() {
+    if (mobileCheck()){
+        document.getElementById("navBar").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+    }
 }
 
 function mobileCheck() {
