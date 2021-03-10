a1a10862f36309140adb87763114499660e1d775
Joeling-M <joseph.myc@gmail.com>
2021-03-09 23:31:23 -0500

    minor html changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
+    console.log($(window).width());
     document.getElementById("navBar").style.width = "20vw";
     document.getElementById("main").style.marginLeft = "20vw";
 }
