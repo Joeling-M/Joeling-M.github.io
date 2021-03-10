9576a13929f3b9ce8318503269ed96619b99f29a
Joeling-M <joseph.myc@gmail.com>
2021-03-10 01:54:14 -0500

    mobile css change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    if (mobileCheck()){
+    if (mobileCheck() || window.innerWidth <= 860){
         document.getElementById("navBar").style.width = "100vw";
     } else {
         document.getElementById("navBar").style.width = "20vw";
